a712ba5fcca90c16b0fc7226e64adf7fada45d2a
Janvi Nandre <nandrejanvi@gmail.com>
2021-10-19 15:50:15 +0530

    birthday reminder added

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'Hinata Shoyo',
+    age: 22,
+    image:
+      'https://s3-alpha.figma.com/hub/file/1067843952/a3ababe3-969e-49f4-b3cb-6acbb724368d-cover.png',
+  },
+  {
+    id: 2,
+    name: 'Gojo Satoru',
+    age: 28,
+    image:
+      'https://i.pinimg.com/originals/50/b0/c3/50b0c37ca8c9f6240144390c25320ee6.jpg',
+  },
+  {
+    id: 3,
+    name: 'Hua Cheng',
+    age: 800,
+    image:
+      'https://i.pinimg.com/736x/be/9d/c1/be9dc122283e3b87fc2eaa051b88a870.jpg',
+  },
+  {
+    id: 4,
+    name: 'Koutaru Bokuto',
+    age: 24,
+    image:
+      'https://i.pinimg.com/originals/c5/40/14/c540140db7db8b89742eb153e5f03168.jpg',
+  },
+];
